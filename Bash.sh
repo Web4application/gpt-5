02bc33cf0bcb8fc33bbf50e9d370f8d6ed991cbb
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-01 15:34:56 +0100

    Update Bash.sh

--- Bash.sh
+++ Bash.sh
@@ -5,3 +5,10 @@ sudo apt-get install libasound2-dev
 sudo dnf install alsa-lib-devel
 sudo pacman -S alsa-lib
 pip install pyalsaaudio
+./configure --node-builtin-modules-path "$(pwd)"
+winget configure .\.configurations\configuration.dsc.yaml
+Set-ExecutionPolicy Unrestricted -Force
+iex ((New-Object System.Net.WebClient).DownloadString('https://boxstarter.org/bootstrapper.ps1'))
+get-boxstarter -Force
+Install-BoxstarterPackage https://raw.githubusercontent.com/nodejs/node/HEAD/tools/bootstrap/windows_boxstarter -DisableReboots
+refreshenv
