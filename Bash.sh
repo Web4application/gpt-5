866f03402de4f6189aef9f9e33a040c6810e0d11
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-01 15:29:51 +0100

    Update Bash.sh

--- Bash.sh
+++ Bash.sh
@@ -1,2 +1,7 @@
 git clone https://github.com/nodejs/node.git
 cd node
+sudo apt-get update
+sudo apt-get install libasound2-dev
+sudo dnf install alsa-lib-devel
+sudo pacman -S alsa-lib
+pip install pyalsaaudio
